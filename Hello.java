644a12ffa61d869bd5ca9d1e6e98d3142679723a
HarishNike <49608687+HarishNike@users.noreply.github.com>
2021-11-16 20:14:23 +0530

    CI

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ public class Hello
 public static void main(String[]asd){
 System.out.println("Hello world");
 System.out.println("this is harish jenkins job");
-System.out.println("after CI");
+System.out.println("after CI/CD");
 }
 }
